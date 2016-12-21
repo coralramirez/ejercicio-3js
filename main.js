f1e19fbf302b7fd3add134aecda64352e28ce5d5
PatriciaMendozas <patriciamendozasoncco@gmail.com>
2016-12-21 10:20:50 -0500

    subo main.js

---
+++ main.js
@@ -0,0 +1,8 @@
+var pago, hora, multiplicar;
+
+pago = prompt("Por favor, ingresar sueldo");
+hora = prompt("Por favor, ingresar hora");
+
+resultado = parseFloat(pago) * parseFloat(hora);
+
+alert(resultado);
